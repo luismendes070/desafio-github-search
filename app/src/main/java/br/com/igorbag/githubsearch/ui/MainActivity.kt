0ce4b1c309cf100c8ada81f3a23d7e81f98f1820
Luis Mendes <machadoluismendes@gmail.com>
2023-08-30 19:49:06 -0300

    Increment
    userName

--- app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
@@ -44,7 +44,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showUserName() {
-        //@TODO 4- depois de persistir o usuario exibir sempre as informacoes no EditText  se a sharedpref possuir algum valor, exibir no proprio editText o valor salvo
+        //@TODO 4- depois de persistir o usuario exibir sempre as informacoes no EditText  se a
+    // sharedpref possuir algum valor, exibir no proprio editText o valor salvo
+
+        nomeUsuario = "luismendes070"
+
     }
 
     //Metodo responsavel por fazer a configuracao base do Retrofit
